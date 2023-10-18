215e465486eecaae2c25dd5aacfe241506afd32d
jruizsilva <ruizsilvajonathan@gmail.com>
2023-10-18 19:09:15 -0300

    first commit

---
+++ src/main/java/twitterclon/TwitterClonApplication.java
@@ -0,0 +1,13 @@
+package twitterclon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterClonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterClonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/twitterclon/TwitterClonApplicationTests.java
@@ -0,0 +1,13 @@
+package twitterclon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterClonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
