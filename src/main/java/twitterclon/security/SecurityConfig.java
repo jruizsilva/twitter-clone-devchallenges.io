faec423eead344f96198a55e909d150f3a159b9f
jruizsilva <ruizsilvajonathan@gmail.com>
2023-10-20 13:09:45 -0300

    example with @EnableMethodSecurity and @PreAuthorize

--- src/main/java/twitterclon/security/SecurityConfig.java
+++ src/main/java/twitterclon/security/SecurityConfig.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,6 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final CustomUserDetailsService customUserDetailsService;
@@ -48,10 +51,11 @@ public class SecurityConfig {
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
             })
             .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers("/auth/**")
+                    .requestMatchers("/api/auth/**")
                     .permitAll()
                     .anyRequest()
-                    .authenticated());
+                    .authenticated())
+            .httpBasic(Customizer.withDefaults());
         http.addFilterBefore(jwtAuthenticationFilter(),
                              UsernamePasswordAuthenticationFilter.class);
         return http.build();
